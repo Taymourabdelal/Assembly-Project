81e3a0d62308ea6fadd3c0522306dbe98de538d0
alyhesham <alyhesham2010@live.com>
2018-03-20 14:29:03 +0200

    zebu

--- Assembly Project/main.cpp
+++ Assembly Project/main.cpp
@@ -12,6 +12,37 @@
 #include "Formats.hpp"
 using namespace std;
 
+
+string BinToHex(string s)
+{
+     string binToHex = "0x";
+for(int i = 0; i < 32; i+=4)
+{
+   string tmp = "0000";
+   
+        tmp = s.substr(i, i+4);
+        if      (!tmp.compare("0000")) binToHex += "0";
+                else if (!tmp.compare("0001")) binToHex += "1";
+                else if (!tmp.compare("0010")) binToHex += "2";
+                else if (!tmp.compare("0011")) binToHex += "3";
+                else if (!tmp.compare("0100")) binToHex += "4";
+                else if (!tmp.compare("0101")) binToHex += "5";
+                else if (!tmp.compare("0110")) binToHex += "6";
+                else if (!tmp.compare("0111")) binToHex += "7";
+                else if (!tmp.compare("1000")) binToHex += "8";
+                else if (!tmp.compare("1001")) binToHex += "9";
+                else if (!tmp.compare("1010")) binToHex += "A";
+                else if (!tmp.compare("1011")) binToHex += "B";
+                else if (!tmp.compare("1100")) binToHex += "C";
+                else if (!tmp.compare("1101")) binToHex += "D";
+                else if (!tmp.compare("1110")) binToHex += "E";
+                else if (!tmp.compare("1111")) binToHex += "F";
+                                else continue;
+
+    }
+
+    return binToHex;
+}
 struct instWord
 {
     string instText;
